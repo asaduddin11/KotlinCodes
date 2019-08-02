83716b771304cc79354cbbc912a772f684043728
Asad Mohammed <asaduddin1199@gmail.com>
2019-08-02 15:04:40 +0100

    Code to find difference of values in a matrix

---
+++ DiagonalDifference.kt
@@ -0,0 +1,30 @@
+import kotlin.math.absoluteValue
+
+fun diagonalDifference(arr: Array<Array<Int>>): Int {
+    // Write your code here
+    var one = IntArray(3)
+    var two = IntArray(3)
+    for (i in 0 until arr.size){
+        one.set(i, arr[i][i])
+    }
+    for(i in 0 until arr.size){
+        var j = arr.size-i -1
+        two.set(i, arr[i][j])
+
+    }
+    return (one.sum() - two.sum()).absoluteValue
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val arr = Array<Array<Int>>(n, { Array<Int>(n, { 0 }) })
+
+    for (i in 0 until n) {
+        arr[i] = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+    }
+
+    val result = diagonalDifference(arr)
+
+    println(result)
+}
